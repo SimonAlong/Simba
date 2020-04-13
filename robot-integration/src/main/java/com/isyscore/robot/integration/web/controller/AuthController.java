aa5064f848fcaf0afa09894e469e318204d3269d
simonalong <simonalong@126.com>
2020-04-13 11:12:58 +0800

    add 添加权限的配置

--- robot-integration/src/main/java/com/isyscore/robot/integration/web/controller/AuthController.java
+++ robot-integration/src/main/java/com/isyscore/robot/integration/web/controller/AuthController.java
@@ -6,13 +6,13 @@ import com.isyscore.os.dev.api.permission.service.PermissionService;
 import com.isyscore.os.dev.util.IsyscoreHashMap;
 import com.isyscore.os.sso.session.RequestUserHolder;
 import com.isyscore.os.sso.session.UserForm;
+import com.isyscore.robot.integration.util.FileUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.ServletRequest;
+import java.io.IOException;
 
 /**
  * @author shizi
@@ -27,7 +27,7 @@ public class AuthController {
     @Autowired
     private PermissionService permissionService;
 
-    // todo 只需要获取三个接口：1.获取用户详情，2.获取权限结构信息（给os使用），3.获取用户权限信息
+    // todo 只需要获取三个接口：1.获取用户详情（可选），2.获取权限结构信息（给os使用），3.获取用户权限信息
 
     @GetMapping("getList")
     public void test1() {
@@ -36,32 +36,49 @@ public class AuthController {
         coreMap.put(SID_STR, currentUser.getToken());
 
         QueryUserAclRequestBuilder builder = new QueryUserAclRequestBuilder()
-        //设置应用 code: 指查询指定应用下的用户权限
-        .setAppCode("appCode")
-        //设置请求头
-        .setHeaders(coreMap);
+            //设置应用 code: 指查询指定应用下的用户权限
+            .setAppCode("appCode")
+            //设置请求头
+            .setHeaders(coreMap);
 
-         QueryUserAclResult result = permissionService.queryUserAcl(builder);
+        QueryUserAclResult result = permissionService.queryUserAcl(builder);
         // 判断结果是否成功
         System.out.println("success: " + result.isSuccess());
         // 获取 httpCode:http 请求结果的 code 码
-         System.out.println("httpCode: " + result.getResponse().getHttpCode());
+        System.out.println("httpCode: " + result.getResponse().getHttpCode());
         // 获取 httpMessage:http 请求结果的消息
-         System.out.println("httpMessage: " + result.getResponse().getHttpMessage());
+        System.out.println("httpMessage: " + result.getResponse().getHttpMessage());
         // 获取 code:接口返回的错误码
-         System.out.println("code: " + result.getResponse().getCode());
+        System.out.println("code: " + result.getResponse().getCode());
         // 获取 message:接口返回的错误消息
-         System.out.println("message: " + result.getResponse().getMessage());
+        System.out.println("message: " + result.getResponse().getMessage());
         // 获取 detail:接口返回的错误附带详情
-         System.out.println("detail: " + result.getResponse().getDetail());
+        System.out.println("detail: " + result.getResponse().getDetail());
         // 获取结果数据
-         System.out.println("data: " + result.getResponse().getBody());
+        System.out.println("data: " + result.getResponse().getBody());
         // 解析结果数据(Json 转 Object)
-         System.out.println("parseData: " + result.parseData());
+        System.out.println("parseData: " + result.parseData());
     }
 
+    /**
+     * 给OS进行使用对应的菜单结构
+     */
     @GetMapping("getAuthListForOs")
-    public String getAuthList(){
+    public String getAuthList() {
+        try {
+            return FileUtil.readFromResource(this.getClass(), "/router/router/json");
+        } catch (IOException e) {
+            return "{}";
+        }
+    }
+
+    /**
+     * 获取用户当前的权限
+     * <p>
+     * 用于在用户请求某个页面的时候，将对应的权限返回给前端
+     */
+    @GetMapping("getAuthOfUser")
+    public String getAuthOfUser() {
         // todo
         return "";
     }
