2abc036e7b2014e3c07e9c10267a8721ffbdf973
simonalong <simonalong@126.com>
2020-04-21 21:20:34 +0800

    add 添加基本的测试模块

---
+++ robot-integration/src/test/java/com/isyscore/robot/integration/RobotApplicationTest.java
@@ -0,0 +1,11 @@
+package com.isyscore.robot.integration;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author shizi
+ * @since 2020/4/21 9:11 PM
+ */
+@SpringBootTest
+public class RobotApplicationTest {
+}
